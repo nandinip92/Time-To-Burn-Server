dee1aeee74cf5d7a52e3b2fa12657fed4fb5fa73
nandinip92 <nandini.p8892@gmail.com>
2023-09-22 14:12:58 +0100

    feat: Getting data from local sequelize DB

--- server/services/exercises.ts
+++ server/services/exercises.ts
@@ -1,31 +1,9 @@
-import { Exercise } from "../models/exercise";
+import { sequelize } from "../database/database";
+import { ExerciseType } from "../types/nutrition.types";
 
-export const getExercises = async () => {
-  return Exercise.findAll();
+export const getExercises = async (): Promise<Array<ExerciseType>> => {
+  const [exerciseInfo, metadata] = await sequelize.query(
+    "SELECT * FROM Exercises"
+  );
+  return exerciseInfo as ExerciseType[];
 };
-
-// export const getExercise = async (exerciseId: number) => {
-// 	return Exercise.findOne({
-// 		where: { exerciseId },
-// 	});
-// };
-
-// export const saveExercise = async (exercise: Exercise) => {
-// 	return Exercise.create<Exercise>(exercise);
-// };
-
-// // User Story 4 - Update Book By Id Solution
-// export const updateExercise = async (exerciseId: number, exercise: Exercise) => {
-// 	return Exercise.update(exercise, {
-// 		where: {
-// 			exerciseId,
-// 		},
-// 	});
-// };
-
-// // Delete Book By Id Solution
-// export const deleteExercise = async (exerciseId: number) => {
-// 	return Exercise.destroy({
-// 		where: { exerciseId },
-// 	});
-// };
