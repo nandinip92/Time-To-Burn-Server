528cc6cb5f9a93c1d94cb9f2eb76f40a831c5746
nandinip92 <nandini.p8892@gmail.com>
2023-09-24 14:13:35 +0100

    feat; Adding code to handle errors

--- server/controllers/nutrition_data_controller.ts
+++ server/controllers/nutrition_data_controller.ts
@@ -9,18 +9,26 @@ export const getNutritionInfo = async (req: Request, res: Response) => {
     res.status(500).send({ message: "Invalid ingredient list" });
     return;
   }
-  const nutrition = await nutritionService.getNutritionData(ingredients);
-  //console.log("nutrition--->", nutrition);
-  //res.status(200).send(nutrition);
-  const total_calories = await nutritionService.getTotalCalories(nutrition);
-  //res.status(200).send(total_calories);
-  const exerciseInfo = await exercisesService.getExercises();
-  await exercisesService.getTimeToBurn(exerciseInfo, total_calories);
-  const result: CalBurnRate = {
-    items: nutrition.items,
-    total_calories: total_calories,
-    exercises: exerciseInfo,
-  };
+  try {
+    const nutrition = await nutritionService.getNutritionData(ingredients);
+    //console.log("Nutrition--->", nutrition);
+    if (nutrition.items.length === 0) {
+      throw new Error("Cannot fetch Nutrition of the items from the API");
+    }
+    const total_calories = await nutritionService.getTotalCalories(nutrition);
+    const exerciseInfo = await exercisesService.getExercises();
+    if (exerciseInfo.length === 0) {
+      throw new Error("Cannot fetch Exercise Info from Sequelize");
+    }
+    await exercisesService.getTimeToBurn(exerciseInfo, total_calories);
+    const result: CalBurnRate = {
+      items: nutrition.items,
+      total_calories: total_calories,
+      exercises: exerciseInfo,
+    };
 
-  res.status(200).send(result);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send({ success: false, message: (error as Error).message });
+  }
 };
