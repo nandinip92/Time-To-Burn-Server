72931ac73ce1badfa568ee516863880592c6283c
nandinip92 <nandini.p8892@gmail.com>
2023-09-27 09:34:04 +0100

    refacotor: Moving the code of calling another service 'exercise' from nutrition controller to nutrition service as a controller should call a single service

--- server/controllers/nutrition_data_controller.ts
+++ server/controllers/nutrition_data_controller.ts
@@ -12,23 +12,36 @@ export const getNutritionInfo = async (req: Request, res: Response) => {
   try {
     const nutrition = await nutritionService.getNutritionData(ingredients);
     //console.log("Nutrition--->", nutrition);
-    if (nutrition.items.length === 0) {
-      throw new Error("Cannot fetch Nutrition of the items from the API");
+    if (typeof nutrition === "string") {
+      throw new Error(nutrition);
     }
-    const total_calories = await nutritionService.getTotalCalories(nutrition);
-    const exerciseInfo = await exercisesService.getExercises();
-    if (exerciseInfo.length === 0) {
-      throw new Error("Cannot fetch Exercise Info from Sequelize");
-    }
-    await exercisesService.getTimeToBurn(exerciseInfo, total_calories);
-    const result: CalBurnRate = {
-      items: nutrition.items,
-      total_calories: total_calories,
-      exercises: exerciseInfo,
-    };
-
-    res.status(200).send(result);
+    res.status(200).send(nutrition);
   } catch (error) {
     res.status(500).send({ success: false, message: (error as Error).message });
   }
 };
+
+/*
+try {
+  const nutrition = await nutritionService.getNutritionData(ingredients);
+  //console.log("Nutrition--->", nutrition);
+  
+  if (nutrition.items.length === 0) {
+    throw new Error("Cannot fetch Nutrition of the items from the API");
+  }
+  const total_calories = await nutritionService.getTotalCalories(nutrition);
+  const exerciseInfo = await exercisesService.getExercises();
+  if (exerciseInfo.length === 0) {
+    throw new Error("Cannot fetch Exercise Info from Sequelize");
+  }
+  await exercisesService.getTimeToBurn(exerciseInfo, total_calories);
+  const result: CalBurnRate = {
+    items: nutrition.items,
+    total_calories: total_calories,
+    exercises: exerciseInfo,
+  };
+
+  res.status(200).send(nutrition);
+} catch (error) {
+  res.status(500).send({ success: false, message: (error as Error).message });
+}*/
--- server/services/nutrition.ts
+++ server/services/nutrition.ts
@@ -1,17 +1,42 @@
 import * as dotenv from "dotenv";
 import fetch from "node-fetch";
-import { IngredientsNutrition, Nutrition } from "../types/nutrition.types";
+import {
+  IngredientsNutrition,
+  Nutrition,
+  CalBurnRate,
+} from "../types/nutrition.types";
 
 import { sampleNutritionData, sampleNutritionData_2 } from "../data/sampleData";
+
+import * as exercisesService from "../services/exercises";
 
 //Function to call the actual API
 export async function getNutritionData(
   ingredients: string
-): Promise<IngredientsNutrition> {
+): Promise<CalBurnRate | string> {
   const url = process.env.API_URL + ingredients;
-  return await getNutritionDataFromAPI(url);
-  //return sampleNutritionData; //Sample data for tesing the functions while developing
+  try {
+    const nutrition = await getNutritionDataFromAPI(url);
+    if (nutrition.items.length === 0) {
+      throw new Error("Cannot fetch Nutrition of the items from the API");
+    }
+    const total_calories = await getTotalCalories(nutrition);
+    const exerciseInfo = await exercisesService.getExercises();
+    if (exerciseInfo.length === 0) {
+      throw new Error("Cannot fetch Exercise Info from Sequelize");
+    }
+    await exercisesService.getTimeToBurn(exerciseInfo, total_calories);
+    const result: CalBurnRate = {
+      items: nutrition.items,
+      total_calories: total_calories,
+      exercises: exerciseInfo,
+    };
+    return result;
+  } catch (error) {
+    return (error as Error).message;
+  }
 }
+
 //Function to call API and get the Nutrition data of the ingredients from the API
 async function getNutritionDataFromAPI(
   url: string
@@ -56,3 +81,13 @@ const getCalories = (ingredient: Nutrition) => {
     .pop();
   return calories;
 };
+
+/*
+export async function getNutritionData(
+  ingredients: string
+): Promise<IngredientsNutrition> {
+  const url = process.env.API_URL + ingredients;
+  return await getNutritionDataFromAPI(url);
+  //return sampleNutritionData; //Sample data for tesing the functions while developing
+}
+*/
