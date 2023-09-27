582c73c67ff544e3efd65ed0aa4e532c6a709ff3
nandinip92 <nandini.p8892@gmail.com>
2023-09-27 14:12:02 +0100

    feat: Adding a teardown() to close the sequelize connection

--- server/database/database.ts
+++ server/database/database.ts
@@ -2,3 +2,6 @@ import { Dialect, Sequelize } from "sequelize";
 // TODO: This should be external config
 
 export let sequelize = new Sequelize("sqlite::memory:");
+export async function teardown() {
+  await sequelize.close();
+}
