06862826274484185dc367ac7df050ae70033f04
nandinip92 <nandini.p8892@gmail.com>
2023-11-03 12:51:46 +0000

    delete: Deleting the database code as we are using the data from file ActivitiesList.json

--- server/database/database.ts
+++
@@ -1,7 +0,0 @@
-import { Dialect, Sequelize } from "sequelize";
-// TODO: This should be external config
-
-export let sequelize = new Sequelize("sqlite::memory:");
-export async function teardown() {
-  await sequelize.close();
-}
