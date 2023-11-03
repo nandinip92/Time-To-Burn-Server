27734d97b51e3a1276541658daa0c1032786de4a
nandinip92 <nandini.p8892@gmail.com>
2023-11-03 13:05:50 +0000

    feat: Adding code to calculate the calories burned per hour for the given specific exercises

---
+++ server/services/calculate_calories_by_weight.ts
@@ -0,0 +1,45 @@
+import { ACTIVITY, CaloriesPerWeight } from "../types/exercise.types";
+
+/*
+ * Following funtion is used to get the calories burned per hour for the given specific exercise category
+ * Calculating calories according to the userWeight
+ *  1. IF the userWeight is in [130, 150, 180, 205] then
+ *        return the calories data the is already stored for that weight
+ *  2. ELSE calculate the calories burned accoring to the given userWeight with reference to the closest value in the standard weights list
+ */
+
+export async function calulateCaloriesPerHourByWeight(
+  exercise: ACTIVITY,
+  userWeight: number
+): Promise<CaloriesPerWeight> {
+  const standardWeights = [130, 155, 180, 205];
+  //If userWeight is in the standardWeights list the assigh that weight
+  // ELSE finding the closest value to the userWeight in the standardWeights list
+  const weight = standardWeights.includes(userWeight)
+    ? userWeight
+    : standardWeights.reduce((prev, curr) =>
+        Math.abs(curr - userWeight) < Math.abs(prev - userWeight) ? curr : prev
+      );
+  const weightWithUnits = weight + "lb";
+  let caloriesPerHour = {};
+
+  if (userWeight === weight) {
+    (Object.keys(exercise) as (keyof typeof exercise)[]).forEach((key) => {
+      caloriesPerHour[key] = exercise[key][weightWithUnits];
+    });
+  } else {
+    (Object.keys(exercise) as (keyof typeof exercise)[]).forEach((key) => {
+      caloriesPerHour[key] = Math.trunc(
+        (exercise[key][weightWithUnits] * userWeight) / weight
+      ) as keyof ACTIVITY;
+    });
+  }
+
+  //console.log("caloriesPerHour:", caloriesPerHour);
+
+  const caloriesPerHourByWeight: CaloriesPerWeight = {
+    userWeight: userWeight + "lb",
+    caloriesPerHour: caloriesPerHour,
+  };
+  return caloriesPerHourByWeight;
+}
