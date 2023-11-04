c0305fc9e9e8bf8571328a96f9fc57f3ad64b804
nandinip92 <nandini.p8892@gmail.com>
2023-11-04 19:54:32 +0000

    refactor: Refactoring the types using 'Record'

--- server/types/exercise.types.ts
+++ server/types/exercise.types.ts
@@ -4,84 +4,101 @@ type CaloriesByWeight = {
   "180lb": number;
   "205lb": number;
 };
-type Cycling = {
-  "Cycling, mountain bike, bmx": CaloriesByWeight | number;
-  "Cycling, <10 mph, leisure bicycling": CaloriesByWeight | number;
-  "Cycling, >20 mph, racing": CaloriesByWeight | number;
-  "Cycling, 10-11.9 mph, light": CaloriesByWeight | number;
-  "Cycling, 12-13.9 mph, moderate": CaloriesByWeight | number;
-  "Cycling, 14-15.9 mph, vigorous": CaloriesByWeight | number;
-  "Cycling, 16-19 mph, very fast, racing": CaloriesByWeight | number;
-  Unicycling: CaloriesByWeight | number;
-  "Stationary cycling, very light": CaloriesByWeight | number;
-  "Stationary cycling, light": CaloriesByWeight | number;
-  "Stationary cycling, moderate": CaloriesByWeight | number;
-  "Stationary cycling, vigorous": CaloriesByWeight | number;
-  "Stationary cycling, very vigorous": CaloriesByWeight | number;
-};
+const CyclingCategories = [
+  "Cycling, mountain bike, bmx",
+  "Cycling, <10 mph, leisure bicycling",
+  "Cycling, >20 mph, racing",
+  "Cycling, 10-11.9 mph, light",
+  "Cycling, 12-13.9 mph, moderate",
+  "Cycling, 14-15.9 mph, vigorous",
+  "Cycling, 16-19 mph, very fast, racing",
+  "Unicycling",
+  "Stationary cycling, very light",
+  "Stationary cycling, light",
+  "Stationary cycling, moderate",
+  "Stationary cycling, vigorous",
+  "Stationary cycling, very vigorous",
+] as const;
+const RunningCategories = [
+  "Running, 5 mph (12 minute mile)",
+  "Running, 5.2 mph (11.5 minute mile)",
+  "Running, 6 mph (10 min mile)",
+  "Running, 6.7 mph (9 min mile)",
+  "Running, 7 mph (8.5 min mile)",
+  "Running, 7.5mph (8 min mile)",
+  "Running, 8 mph (7.5 min mile)",
+  "Running, 8.6 mph (7 min mile)",
+  "Running, 9 mph (6.5 min mile)",
+  "Running, 10 mph (6 min mile)",
+  "Running, 10.9 mph (5.5 min mile)",
+  "Running, cross country",
+  "Running, general",
+  "Running, on a track, team practice",
+  "Running, stairs, up",
+  "Track and field (shot, discus)",
+  "Track and field (high jump, pole vault)",
+  "Track and field (hurdles)",
+] as const;
+const WalkingCategories = [
+  "Walking, under 2.0 mph, very slow",
+  "Walking 2.0 mph, slow",
+  "Walking 2.5 mph",
+  "Walking 3.0 mph, moderate",
+  "Walking 3.5 mph, brisk pace",
+  "Walking 4.0 mph, very brisk",
+  "Walking 4.5 mph",
+  "Walking 5.0 mph",
+] as const;
 
-type Running = {
-  "Running, 5 mph (12 minute mile)": CaloriesByWeight | number;
-  "Running, 5.2 mph (11.5 minute mile)": CaloriesByWeight | number;
-  "Running, 6 mph (10 min mile)": CaloriesByWeight | number;
-  "Running, 6.7 mph (9 min mile)": CaloriesByWeight | number;
-  "Running, 7 mph (8.5 min mile)": CaloriesByWeight | number;
-  "Running, 7.5mph (8 min mile)": CaloriesByWeight | number;
-  "Running, 8 mph (7.5 min mile)": CaloriesByWeight | number;
-  "Running, 8.6 mph (7 min mile)": CaloriesByWeight | number;
-  "Running, 9 mph (6.5 min mile)": CaloriesByWeight | number;
-  "Running, 10 mph (6 min mile)": CaloriesByWeight | number;
-  "Running, 10.9 mph (5.5 min mile)": CaloriesByWeight | number;
-  "Running, cross country": CaloriesByWeight | number;
-  "Running, general": CaloriesByWeight | number;
-  "Running, on a track, team practice": CaloriesByWeight | number;
-  "Running, stairs, up": CaloriesByWeight | number;
-  "Track and field (shot, discus)": CaloriesByWeight | number;
-  "Track and field (high jump, pole vault)": CaloriesByWeight | number;
-  "Track and field (hurdles)": CaloriesByWeight | number;
-};
-type Walking = {
-  "Walking, under 2.0 mph, very slow": CaloriesByWeight | number;
-  "Walking 2.0 mph, slow": CaloriesByWeight | number;
-  "Walking 2.5 mph": CaloriesByWeight | number;
-  "Walking 3.0 mph, moderate": CaloriesByWeight | number;
-  "Walking 3.5 mph, brisk pace": CaloriesByWeight | number;
-  "Walking 4.0 mph, very brisk": CaloriesByWeight | number;
-  "Walking 4.5 mph": CaloriesByWeight | number;
-  "Walking 5.0 mph": CaloriesByWeight | number;
-};
-type Swimming = {
-  "Swimming laps, freestyle, fast": CaloriesByWeight | number;
-  "Swimming laps, freestyle, slow": CaloriesByWeight | number;
-  "Swimming backstroke": CaloriesByWeight | number;
-  "Swimming breaststroke": CaloriesByWeight | number;
-  "Swimming butterfly": CaloriesByWeight | number;
-  "Swimming leisurely, not laps": CaloriesByWeight | number;
-  "Swimming sidestroke": CaloriesByWeight | number;
-  "Swimming synchronized": CaloriesByWeight | number;
-  "Swimming, treading water, fast, vigorous": CaloriesByWeight | number;
-  "Swimming, treading water, moderate": CaloriesByWeight | number;
-  "Water aerobics, water calisthenics": CaloriesByWeight | number;
-  "Water polo": CaloriesByWeight | number;
-  "Water volleyball": CaloriesByWeight | number;
-  "Water jogging": CaloriesByWeight | number;
-  "Diving, springboard or platform": CaloriesByWeight | number;
-};
-type AerobicsDancing = {
-  "Aerobics, low impact": CaloriesByWeight | number;
-  "Aerobics, high impact": CaloriesByWeight | number;
-  "Aerobics, step aerobics": CaloriesByWeight | number;
-  "Aerobics, general": CaloriesByWeight | number;
-  Jazzercise: CaloriesByWeight | number;
-  "Stretching, hatha yoga": CaloriesByWeight | number;
-  "Mild stretching": CaloriesByWeight | number;
-  "Instructing aerobic class": CaloriesByWeight | number;
-  "Water aerobics": CaloriesByWeight | number;
-  "Ballet, twist, jazz, tap": CaloriesByWeight | number;
-  "Ballroom dancing, slow": CaloriesByWeight | number;
-  "Ballroom dancing, fast": CaloriesByWeight | number;
-  "Teach aerobic classes (& participate)": CaloriesByWeight | number;
-};
+const SwimmingCategories = [
+  "Swimming laps, freestyle, fast",
+  "Swimming laps, freestyle, slow",
+  "Swimming backstroke",
+  "Swimming breaststroke",
+  "Swimming butterfly",
+  "Swimming leisurely, not laps",
+  "Swimming sidestroke",
+  "Swimming synchronized",
+  "Swimming, treading water, fast, vigorous",
+  "Swimming, treading water, moderate",
+  "Water aerobics, water calisthenics",
+  "Water polo",
+  "Water volleyball",
+  "Water jogging",
+  "Diving, springboard or platform",
+] as const;
+
+const AerobicsDancingCategories = [
+  "Aerobics, low impact",
+  "Aerobics, high impact",
+  "Aerobics, step aerobics",
+  "Aerobics, general",
+  "Jazzercise",
+  "Stretching, hatha yoga",
+  "Mild stretching",
+  "Instructing aerobic class",
+  "Water aerobics",
+  "Ballet, twist, jazz, tap",
+  "Ballroom dancing, slow",
+  "Ballroom dancing, fast",
+  "Teach aerobic classes (& participate)",
+];
+
+type CyclingCategoryType = (typeof CyclingCategories)[number];
+type RunningCategoryType = (typeof RunningCategories)[number];
+type WalkingCategoryType = (typeof WalkingCategories)[number];
+type SwimmingCategoriyType = (typeof SwimmingCategories)[number];
+type AerobicsDancingCategoryType = (typeof AerobicsDancingCategories)[number];
+
+// type Cycling = {
+//   CyclingCategoryType: CaloriesByWeight;
+// };
+type Cycling = Record<CyclingCategoryType, CaloriesByWeight>;
+type Running = Record<RunningCategoryType, CaloriesByWeight>;
+type Walking = Record<WalkingCategoryType, CaloriesByWeight>;
+type Swimming = Record<SwimmingCategoriyType, CaloriesByWeight>;
+type AerobicsDancing = Record<AerobicsDancingCategoryType, CaloriesByWeight>;
+
 export type ExercisesType = {
   Cycling: Cycling;
   Running: Running;
