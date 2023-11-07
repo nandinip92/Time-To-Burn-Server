cebbf9727d5dcf239f34846cc58c260ef3d182b5
nandinip92 <nandini.p8892@gmail.com>
2023-11-07 18:13:42 +0000

    refactor: Changing the variable names to snake_case

--- server/services/calculate_time.ts
+++ server/services/calculate_time.ts
@@ -1,27 +1,30 @@
 import { ACTIVITY, CaloriesPerWeight } from "../types/exercise.types";
+import { CaloriesDataType } from "../types/nutrition.types";
 import { calulateCaloriesPerHourForExerciseByWeight } from "./calculate_calories_by_weight";
 import { calculateTimeToBurnTotalCalories } from "./calculate_time2burn_total_calories";
 
 export async function calculateTime(
-  total_calories: number,
+  ingredient_calories: CaloriesDataType,
   exercise: ACTIVITY,
-  userWeight: number
+  user_weight: number
 ): Promise<string> {
   console.log("-------------calculateTime---------------");
   //   console.log(
   //     `total_calories: ${total_calories}\nexercise: ${JSON.stringify(
   //       exercise
-  //     )}\nweight: ${userWeight}`
+  //     )}\nweight: ${user_weight}`
   //   );
-  //   const caloriesPerHourPerExercise = await calulateCaloriesPerHourByWeight(
+  //   const calories_per_hour_for_exercise = await calulateCaloriesPerHourByWeight(
   //     exercise,
-  //     userWeight
+  //     user_weight
   //   );
-  const caloriesPerHourPerExercise =
-    await calulateCaloriesPerHourForExerciseByWeight(exercise, userWeight);
-  const timetoBurnTotalCalories = await calculateTimeToBurnTotalCalories(
+  const calories_per_hour_for_exercise =
+    await calulateCaloriesPerHourForExerciseByWeight(exercise, user_weight);
+
+  const total_calories = ingredient_calories.total_calories;
+  const time_to_burn_total_calories = await calculateTimeToBurnTotalCalories(
     total_calories,
-    caloriesPerHourPerExercise
+    calories_per_hour_for_exercise
   );
   return "";
 }
