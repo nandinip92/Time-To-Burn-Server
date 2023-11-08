2ac66bd6e3de2411f37643e9943c5d6d32a0eec9
nandinip92 <nandini.p8892@gmail.com>
2023-11-08 19:49:21 +0000

    feat: Implementing code to get the time to burn data

--- server/services/get_time_to_burn.ts
+++ server/services/get_time_to_burn.ts
@@ -1,25 +1,33 @@
 import { ACTIVITY, ActivityCaloriesPerWeight } from "../types/exercise.types";
 import { CaloriesDataType } from "../types/nutrition.types";
+import { Time, TimeToBurn, TimeToBurnPerActivity } from "../types/time.types";
 import { calulateCaloriesPerHourForExerciseByWeight } from "./calculate_calories_by_weight";
 import { calculateTimeToBurnTotalCalories } from "./calculate_time2burn_total_calories";
-
+/*
+ * Tis funtion will to the following
+ * 1. will calculate the calories burned per hour for exercise for the given userWeight
+ *        |-> Check-> calulateCaloriesPerHourForExerciseByWeight
+ * 2.Taking the exerciseCalories calculated from the above and the totalCalories together as input to
+ * 'calculateTimeToBurnTotalCalories', this will return the timeToBurnTotalCalories
+ *
+ */
 export async function getTimeToBurn(
   ingredientCalories: CaloriesDataType,
   exercise: ACTIVITY,
   userWeight: number
-): Promise<string> {
+): Promise<TimeToBurnPerActivity | string> {
   try {
     const exerciseCaloriesAndWeight =
       await calulateCaloriesPerHourForExerciseByWeight(exercise, userWeight);
 
     const totalCalories = ingredientCalories.totalCalories;
     const caloriesPerHour = exerciseCaloriesAndWeight.caloriesPerHour;
-    const timeToBurnTotalCalories = await calculateTimeToBurnTotalCalories(
-      totalCalories,
-      caloriesPerHour
-    );
-    console.log("timeToBurnTotalCalories", timeToBurnTotalCalories);
-    return "";
+    const timeToBurnTotalCalories: TimeToBurnPerActivity =
+      await calculateTimeToBurnTotalCalories(totalCalories, caloriesPerHour);
+
+    console.log("exerciseCaloriesAndWeight", exerciseCaloriesAndWeight);
+    //console.log("timeToBurnTotalCalories", timeToBurnTotalCalories);
+    return timeToBurnTotalCalories;
   } catch (error) {
     return (error as Error).message;
   }
