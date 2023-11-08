4ed2d660110867c8b05e672c32b5e568299be514
nandinip92 <nandini.p8892@gmail.com>
2023-11-08 20:05:42 +0000

    refactor: uncommenting the API call and commening the static data return

--- server/services/nutrition.ts
+++ server/services/nutrition.ts
@@ -3,28 +3,28 @@ import fetch from "node-fetch";
 import {
   IngredientsNutrition,
   Nutrition,
-  CalBurnRate,
+  TotalCaloriesWithNutirion,
 } from "../types/nutrition.types";
 
 import { sampleNutritionData, sampleNutritionData_2 } from "../data/sampleData";
 
-import * as exercisesService from "./exercises_and_calories";
+import * as exercisesService from "./calories_for_exercises";
 import { calculateTotalCalories } from "./calculate_total_calories";
 
 //Function to call the actual API
 export async function getNutritionData(
   ingredients: string
-): Promise<CalBurnRate | string> {
+): Promise<TotalCaloriesWithNutirion | string> {
   const url = process.env.API_URL + ingredients;
   try {
     const nutrition = await getNutritionDataFromAPI(url);
     if (nutrition.items.length === 0) {
       throw new Error("Cannot fetch Nutrition of the items from the API");
     }
-    const total_calories = await calculateTotalCalories(nutrition);
-    const result: CalBurnRate = {
+    const totalCalories = await calculateTotalCalories(nutrition);
+    const result: TotalCaloriesWithNutirion = {
       items: nutrition.items,
-      total_calories: total_calories,
+      totalCalories: totalCalories,
     };
     return result;
   } catch (error) {
@@ -57,5 +57,5 @@ async function getNutritionDataFromAPI(
 // ): Promise<IngredientsNutrition> {
 //   const url = process.env.API_URL + ingredients;
 //   //return await getNutritionDataFromAPI(url);
-//   return sampleNutritionData; //Sample data for tesing the functions while developing
+//   return sampleNutritionData_2; //Sample data for tesing the functions while developing
 // }
