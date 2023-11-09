5d5d9e16ca20f8c00bf7b7f69667399f5c18dadf
nandinip92 <nandini.p8892@gmail.com>
2023-11-09 10:58:43 +0000

    feat: Adding error handling

--- server/controllers/exercise_data_contoller.ts
+++ server/controllers/exercise_data_contoller.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
-import * as exercisesService from "../services/exercises_and_calories";
+import * as exercisesService from "../services/calories_burned_during_exercises";
 
 export const getExercisesInfo = async (req: Request, res: Response) => {
   //const exercise = req.params.exercise;
   try {
     const exercises = await exercisesService.getExercisesData();
+    if (typeof exercises === "string") {
+      throw new Error(exercises);
+    }
     res.send(exercises);
   } catch (error) {
     res.status(500).send({ success: false, message: (error as Error).message });
